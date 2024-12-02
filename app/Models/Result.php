4dfeb4cfe8b05c426b14f0bfa84cfdae57f476d2
Dana <danadev94@gmail.com>
2024-12-02 14:32:50 +0000

    Add NULL return type to getTitle() on App\Models\Result

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -10,7 +10,7 @@ class Result extends Model
 
     protected $fillable = ['process_attempt_id', 'parsable', 'type', 'movie_info', 'series_info', 'clip_info'];
 
-    public function getTitle(): string
+    public function getTitle(): null|string
     {
         if ($this->type == 'movie') {
             return json_decode($this->movie_info, 1)['movie_name'];
